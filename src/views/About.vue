73d64b6b99267070b4522414f263a93c9798c48d
z-sohaib <js_zouambia@esi.dz>
2020-12-20 11:02:54 +0100

    Project Files

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is my first app made with Vue.js framework following Traversy Media course</p>
+    <p>It is a Todolist app where you can add, edit and remove tasks</p>
+    <p>Follow me for more</p>
+  </div>
+</template>
+
+<style scoped>
+* {
+  text-align: center;
+}
+</style>
